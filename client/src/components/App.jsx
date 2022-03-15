ec1e11c7eb2029dabeeec2e865b518aec19f29d8
Samantha Pham <samanthavpham@gmail.com>
2022-03-15 16:13:42 -0700

    succesful api call using input from app

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,14 +1,87 @@
 import React from 'react';
+const axios = require('axios');
+const config = require('../../../config');
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      city: '',
+      stateCode: '',
+      countryCode: '',
+      location: '',
+      weather: '',
+      temp: 0
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    if (this.state.countryCode === '') {
+      axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${this.state.city},${this.state.countryCode}&appid=${config.app.openWeatherApiKey}&units=imperial`)
+      .then((response) => {
+        console.log(response.data);
+        this.setState({
+          city: '',
+          stateCode: '',
+          countryCode: '',
+          location: response.data.name,
+          weather: response.data.weather[0].description,
+          temp: response.data.main.temp
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    } else {
+      axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${this.state.city},${this.state.stateCode},${this.state.countryCode}&appid=${config.app.openWeatherApiKey}&units=imperial`)
+      .then((response) => {
+        console.log(response.data);
+        this.setState({
+          city: '',
+          stateCode: '',
+          countryCode: '',
+          location: response.data.name,
+          weather: response.data.weather[0].description,
+          temp: response.data.main.temp
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    }
   }
 
   render() {
     return (
-      <h1>WeatherWear</h1>
+      <div>
+        <h1>WeatherWear</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            City:
+            <input type='text' name='city' onChange={this.handleChange} />
+          </label>
+          <label>
+            State Code:
+            <input type='text' name='stateCode' onChange={this.handleChange} />
+          </label>
+          <label>
+            Country Code:
+            <input type='text' name='countryCode' onChange={this.handleChange} />
+          </label>
+          <input type='submit' value='Submit' />
+        </form>
+        <h3>{`Temperature in ${this.state.location} is ${Math.round(this.state.temp)} degrees F and ${Math.round((this.state.temp - 32) * (5 / 9))} degrees C, ${this.state.weather}`}</h3>
+      </div>
     );
   }
 }
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const PORT = process.env.PORT || 3000;
+const config = require('../config');
+const PORT = config.server.port;
 const app = express();
 const morgan = require('morgan');
 const cors = require('cors')
